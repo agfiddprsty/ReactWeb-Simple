b45fe9b47de7811a1aa4ff89ab42f1163566a5b8
Muhammad Ardhana Wahyu Nugraha Muhammad Ardhana Wahyu Nugraha <mardhanawn@gmail.com>
2018-12-18 18:04:31 +0700

    Profile

---
+++ src/Profile.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Profile extends Component {
+    render() {
+        return (
+          <div>
+              <header />
+                <body>
+                    <p>Ini adalah tampilan Profile</p>
+                </body>
+              <footer />
+          </div> 
+        );
+    }
+}
+
+export default Profile;
