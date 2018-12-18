c2a0239a88c1af203683344516ce2c0ff1c45084
Tsaqif Setyo <tsaqiftrooper@gmail.com>
2018-12-18 17:56:25 +0700

    add new tab

---
+++ src/Menu.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Menu.css';
+
+class Menu extends Component {
+  render() {
+    return (
+      <div className="Hom">
+        <header className="Hom-header">
+            <nav>
+              <ul>
+                <li><a href="#">HOME</a></li>
+                 <li><a href="#">ABOUT</a></li>
+                  <li><a href="#">USER</a></li>
+               
+              </ul>
+            </nav>
+</header>
+<body>
+ini boddy
+</body>
+<footer className="App-footer">
+          <div> ini footer </div>
+          <div> COPYRIGHT 2018 </div>
+        </footer>
+</div>
+    );
+  }
+}
+export default Menu;
