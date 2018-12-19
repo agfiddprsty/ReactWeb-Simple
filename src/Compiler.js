25e07c06f04835fa5024a480262cb0feef20816f
Ramadhani Samudra Gawang Indiyanto <ganar.gatul@gmail.com>
2018-12-19 13:53:30 +0700

    add compiler widget

---
+++ src/Compiler.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Compiler extends Component{
+    render(){
+        return(
+          <div class="sec-widget" data-widget="ae8b0e424f8131fe9111197ab23a1e2c"></div>
+        );
+    }
+}
