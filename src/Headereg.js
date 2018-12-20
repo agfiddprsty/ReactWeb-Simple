82456dedff3f5c38bffb844878d54f62df1fafb7
Agfid Danu Prasetyo <3103117235@student.smktelkom-pwt.sch.id>
2018-12-20 17:23:51 +0700

    update header add form password update relative regist update dashboard

---
+++ src/Headereg.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Home.css';
+import {withRouter,Link} from 'react-router-dom';
+class Headereg extends Component {
+  render() {
+    return (
+      <div className="Hom">
+        <header className="Hom-header">
+            <nav>
+              <ul>
+                <li><Link to='/Reg' className="nav-link">REGISTER</Link></li>
+                <li><a href="#" className="nav-link">ABOUT</a></li>
+              </ul>
+            </nav>
+        </header>
+      </div>
+    );
+  }
+}
+export default Headereg;
--- src/Registrasi.js
+++ src/Registrasi.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import ReactDOM from 'react-dom';
 import { ReCaptcha } from 'react-recaptcha-google'
-import Header from './Header.js';
+import Header from './Headereg.js';
 import Footer from './Footer.js';
 import {Link,Redirect,withRouter} from 'react-router-dom'
 import Home from './Home';
@@ -39,7 +39,7 @@ class registrasi extends Component{
           
         }else{
           // alert('Please Proof You Are Human')
-          this.props.history.push("/Hack");
+          this.props.history.push("/Dashboard");
         //alert(this.state.name);
         }
       }
@@ -84,6 +84,13 @@ class registrasi extends Component{
                 <label className="label-regis">Email</label>
                 <div className="col-9">
                 <input type="email" placeholder="Email" className="form-control"></input>
+                </div>
+              </div>
+
+              <div className="colom">
+                <label className="label-regis">Password</label>
+                <div className="col-9">
+                <input type="password" placeholder="Password" className="form-control"></input>
                 </div>
               </div>
               
