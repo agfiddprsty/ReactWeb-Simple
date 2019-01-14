0eb4f79476c91cfa03085f229b5fb6236ab4c407
Tsaqif Setyo <tsaqiftrooper@gmail.com>
2019-01-14 10:21:15 +0700

    remove horizontal scrolling in home

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -23,7 +23,7 @@ class Home extends Component {
         width: '735px',
         left: '45%',
         position: 'absolute',
-        overflow: 'hidden',
+        overflowx: 'hidden'
     }
 
     var h1 = {
@@ -77,6 +77,7 @@ class Home extends Component {
                     position:'relative',
                     backgroundColor:'#685AA7',
                     borderColor:'#685AA7'
+                    
                 }}>Hire Me</button></a>     }
 
                 <div style={{
