d1ea087743f9c8b608f18fc9f3d7a9d2c64e0865
Agfid Danu Prasetyo <3103117235@student.smktelkom-pwt.sch.id>
2019-01-23 15:02:24 +0700

    add jumbotron at listbatch page

--- src/Screen/ListBatch.js
+++ src/Screen/ListBatch.js
@@ -3,7 +3,6 @@ import Header from './HeaderSilabus';
 import Footer from './Footer';
 import Batch from './batch.js';
 import ModalProfile from './ModalProfile';
-import './css/App.css'
 
 class ListBatch extends React.Component {
   constructor(){
@@ -21,68 +20,85 @@ handleChange(event) {
 
   render() {
     return (
-      <div className="Hom">
-        <Header />
-        <body className="Hom-body">       
-        <section id="batch">
-        <h5 className="listboot">List Batch Bootcamp</h5>
-        <div class="row cardbatch">
-            <div class="col"> <a href='#' data-toggle="modal" data-target="#myModal" className="listbatch">
-            <Batch /></a>
+        <div className="Hom">
+            <div>
+                <Header />
             </div>
-            <div class="col"> <a href='#' data-toggle="modal" data-target="#myModal" className="listbatch">
-            <Batch /></a>
+            <div>
+                <body className="Hom-body">
+                    <div class="jumbotron jumbotron-fluid tron">
+                        <div class=" troncard" style={{width:"100%", height: "500px"}}>
+                                <h4 class="card-title title">Bootcamp Dumbways</h4>
+                                <p class="card-text isi">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut lectus dictum, feugiat arcu laoreet, viverra dolor. Cras et pharetra nulla, id posuere justo.</p>
+                                <a href="#batch" class="btn btn-warning tombol">Register Bootcamp</a>
+                        </div>
+                    </div>
+                    
+                    <div className="divbatch">
+                        <div><br/>
+                            <section className="secbatch" id="batch">
+                                <h5 className="listboot">List Batch Bootcamp</h5>
+                                <div class="row cardbatch">
+                                    <div class="col"> <a href='#' data-toggle="modal" data-target="#myModal" className="listbatch">
+                                        <Batch /></a>
+                                    </div>
+                                    <div class="col"> <a href='#' data-toggle="modal" data-target="#myModal" className="listbatch">
+                                        <Batch /></a>
+                                    </div>
+                                </div>
+                                <div class="row cardbatch">
+                                    <div class="col"> <a href='#' data-toggle="modal" data-target="#myModal" className="listbatch">
+                                        <Batch /></a>
+                                    </div>
+                                    <div class="col"> <a href='#' data-toggle="modal" data-target="#myModal" className="listbatch">
+                                        <Batch /></a>
+                                    </div>
+                                </div>
+                                <div class="row cardbatch">
+                                    <div class="col"> <a href='#' data-toggle="modal" data-target="#myModal" className="listbatch">
+                                        <Batch /></a>
+                                    </div>
+                                    <div class="col"> <a href='#' data-toggle="modal" data-target="#myModal" className="listbatch">
+                                        <Batch /></a>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </body>
             </div>
-        </div>
-        <div class="row cardbatch">
-            <div class="col"> <a href='#' data-toggle="modal" data-target="#myModal" className="listbatch">
-            <Batch /></a>
+            <div>
+                <Footer />
             </div>
-            <div class="col"> <a href='#' data-toggle="modal" data-target="#myModal" className="listbatch">
-            <Batch /></a>
-            </div>
-        </div>
-        <div class="row cardbatch">
-            <div class="col"> <a href='#' data-toggle="modal" data-target="#myModal" className="listbatch">
-            <Batch /></a>
-            </div>
-            <div class="col"> <a href='#' data-toggle="modal" data-target="#myModal" className="listbatch">
-            <Batch /></a>
-            </div>
-        </div>
-        </section>
-        </body>
-        <Footer />
-        <div>
-            <div class="modal fade" id="myModal">
-            <div class="modal-dialog modal-lg">
-                <div class="modal-content">
+            <div>
+                <div class="modal fade" id="myModal">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
 
                 {/* <!-- Modal Header --> */}
-                <div class="modal-header">
-                    <h4 class="modal-title">Confirm</h4>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
+                            <div class="modal-header">
+                                <h4 class="modal-title">Confirm</h4>
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            </div>
 
                 {/* <!-- Modal body --> */}
-                <div class="modal-body"> 
-                    <div>
-                        <ModalProfile />
-                    </div>
-                </div>
+                            <div class="modal-body"> 
+                                <div>
+                                    <ModalProfile />
+                                </div>
+                            </div>
 
                 {/* <!-- Modal footer --> */}
-                <div class="modal-footer">
-                    <a href="/hack"><button type="button" class="btn btn-success">Submit</button></a>
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            <div class="modal-footer">
+                                <a href="/hack"><button type="button" class="btn btn-success">Submit</button></a>
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                </div>
-            </div>
             </div>
         </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 export default ListBatch;
