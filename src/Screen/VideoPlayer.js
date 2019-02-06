9d5fd5282ea04cdb8c11a246a1b03650be7b85cd
Ramadhani Samudra Gawang Indiyanto <ganar.gatul@gmail.com>
2019-02-06 11:33:30 +0700

    add thumbnail videoplayer

--- src/Screen/VideoPlayer.js
+++ src/Screen/VideoPlayer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Player from 'react-player'
-
+import Fb from './img/fb.png'
 class VideoPlayer extends Component{
     
     render(){
@@ -10,9 +10,13 @@ class VideoPlayer extends Component{
             url='http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'
             controls
             playing
+            light={Fb}
+            width='100%'
+            height='100%'
             config={{ 
                 file: { 
                 attributes: {
+                    
                     controlsList: 'nodownload',
                     onContextMenu: e => e.preventDefault()
                 } 
