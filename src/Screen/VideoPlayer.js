2ee2e4fcd0d5c80d0fe717aff9fffcd3ff0391fa
Ramadhani Samudra Gawang Indiyanto <ganar.gatul@gmail.com>
2019-02-06 11:53:55 +0700

    pause onclick

--- src/Screen/VideoPlayer.js
+++ src/Screen/VideoPlayer.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 import Player from 'react-player'
-import Fb from './img/fb.png'
+import Fb from './img/bg.svg'
+// import ''
 class VideoPlayer extends Component{
-    
+    constructor(){
+        super()
+        this._Pause = this._Pause.bind(this)
+        this.state={
+            playing:true
+        }
+    }
+    _Pause(){
+        this.setState({ playing: !this.state.playing })
+    }
     render(){
        
         return(
             <Player
             url='http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'
             controls
-            playing
+            playing={this.state.playing}
             light={Fb}
-            width='100%'
-            height='100%'
+            onClick={this._Pause}
             config={{ 
                 file: { 
                 attributes: {
-                    
+                    style: { width: '50%', height: '50%' },
                     controlsList: 'nodownload',
-                    onContextMenu: e => e.preventDefault()
+                    onContextMenu: e => e.preventDefault(),
                 } 
                 } 
             }}
