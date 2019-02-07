570a51c91c971d15e22f98012cc1d893886d9ff8
Ramadhani Samudra Gawang Indiyanto <ganar.gatul@gmail.com>
2019-02-07 10:48:37 +0700

    add dashboardlogin + header

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ import ProfileRaport from './Screen/ProfileRaport';
 import Payment from './Screen/Payment';
 import Video from './Screen/VideoPlayer';
 import ListVideo from './Screen/ListVideo'
-
+import Dashboard from './Screen/Dashboard'
 //import store 
 import store from './store'
 ReactDOM.render(
@@ -51,6 +51,8 @@ ReactDOM.render(
             <Route path="/payment" component={Payment}/>
             <Route path="/video" component={Video}/>
             <Route path="/listvideo" component={ListVideo}/>
+            <Route path="/Dashboard" component={Dashboard}/>
+
         </div>
         </Provider>
     </Router>
