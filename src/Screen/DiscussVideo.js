888e7308a7037e58536b8c9eb298843c5473d62a
Ramadhani Samudra Gawang Indiyanto <ganar.gatul@gmail.com>
2019-02-11 11:22:35 +0700

    add discuss video on videoplayer delete discussvideo on detailvideo delete button like and reply on discussvideo

--- src/Screen/DiscussVideo.js
+++ src/Screen/DiscussVideo.js
@@ -20,29 +20,29 @@ class DiscussVideo extends Component{
                     Diskusi Yuk...
                 </h3>
                 <div  >
-                    <div class="row" id="post-review-box" style={{marginRight:"29px"}}>
-                        <div class="col-md-8">
-                            <textarea class="form-control animated" cols="50" id="new-review" name="comment" placeholder="Enter your question here..." rows="5"></textarea>
-                            <button class="float-right btn btn-outline-primary ml-2 "> Submit</button>
+                    <div class="row" id="post-review-box" >
+                        <div class="col-md-auto">
+                            <textarea class="form-control animated" cols="150" id="new-review" name="comment" placeholder="Enter your question here..." rows="5" style={{width:"800px"}}></textarea>
+                            <button class=" btn btn-outline-primary"style={{marginLeft:"700px"}} > Submit</button>
                         </div>
                     </div>
                 </div>
               
                 <div class="row"> 
-                    <div class="col-sm-1">
+                    <div class="col-sm-2">
                         <div class="thumbnail">
                             <img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png"/>
                         </div>
                     </div>
-                    <div class="col-sm-5">
+                    <div class="col-sm-10">
                         <div class="panel panel-default">
                             <div class="panel-heading">
                                 <strong>myusername</strong> <span class="text-muted">commented 5 days ago</span>
                             </div>
                             <div class="panel-body">
                                 How To Fast Learning ?
-                                <button class="float-right btn btn-outline-primary ml-2 fa fa-heart"> Like</button>
-                                <button class="float-right btn btn-outline-primary ml-2 fa fa-reply" onClick={this.showreply} > Reply</button>
+                                {/* <button class=" btn btn-outline-primary ml-2 fa fa-heart"> Like</button>
+                                <button class=" btn btn-outline-primary ml-2 fa fa-reply" onClick={this.showreply} > Reply</button> */}
                                 <div id="reply" style={{display: this.state.show ? 'block' : 'none'}}>
                                     <div class="row" id="post-review-box">
                                         <div class="col-md-12">
@@ -53,16 +53,15 @@ class DiscussVideo extends Component{
                             </div>
                         </div>
                     </div>
-
                 </div>
                 <br/>
                 <div class="row">
-                    <div class="col-sm-1">
+                    <div class="col-sm-2">
                         <div class="thumbnail">
                             <img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png"/>
                         </div>
                     </div>
-                    <div class="col-sm-5">
+                    <div class="col-sm-10">
                         <div class="panel panel-default">
                             <div class="panel-heading">
                                 <strong>yourusername</strong> <span class="text-muted">commented 5 days ago</span>
@@ -70,8 +69,8 @@ class DiscussVideo extends Component{
                             <div class="panel-body">
                                 <p>Reply from myusername</p>
                                 Just Try Anytime :V
-                                <button class="float-right btn btn-outline-primary ml-2 fa fa-heart"> Like</button>
-                                <button class="float-right btn btn-outline-primary ml-2 fa fa-reply" onClick={this.showreply} > Reply</button>
+                                {/* <button class="float-right btn btn-outline-primary ml-2 fa fa-heart"> Like</button>
+                                <button class="float-right btn btn-outline-primary ml-2 fa fa-reply" onClick={this.showreply} > Reply</button> */}
                             </div>
                         </div>
                     </div>
