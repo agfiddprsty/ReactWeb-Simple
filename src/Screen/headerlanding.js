a625d378baeb4e7438d6279eeda2fe04514fe60a
Agfid Danu Prasetyo <3103117235@student.smktelkom-pwt.sch.id>
2019-02-11 16:38:07 +0700

    update ui landing page

--- src/Screen/headerlanding.js
+++ src/Screen/headerlanding.js
@@ -8,7 +8,9 @@ class Header extends Component {
       <div className="Hom">
         <header className="Hom-header bg-light">
             <nav className="navbar navbar-light navbar-expand-sm navbar-toggleable-sm fixed-top bg-light">
-              <a class="brand" href="#" style={{fontWeight: '600'}}>DumbWays.id Online Bootcamp</a>
+              <a class="brand" href="#">
+              <img alt="" src="https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=height:60/https://www.filepicker.io/api/file/mWcP190oTCqiAo5VjrE8"/>
+              </a>
 
               {/* <!-- Toggler/collapsibe Button --> */}
               <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
@@ -17,10 +19,10 @@ class Header extends Component {
               <div class="collapse navbar-collapse bg-light colap" id="collapsibleNavbar">
               <ul class="navbar-nav ml-auto">
                 <li className="nav-item ml-auto">
-                  <a href='/' className="nav-link">Sign In</a>
+                  <a href='/' className="nav-link" style={{fontWeight: '600'}}>Sign In</a>
                 </li>
                 <li className="nav-item ml-auto">
-                  <a href="/reg" className="nav-link">Sign Up</a>
+                  <a href="/daftar" className="nav-link active" style={{fontWeight: '600'}}>Sign Up</a>
                 </li>
               </ul>
             </div> 
