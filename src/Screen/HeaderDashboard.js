1292e8afb4b49b143f4e36d338b0a3f14a51b18c
Ramadhani Samudra Gawang Indiyanto <ganar.gatul@gmail.com>
2019-02-11 16:43:33 +0700

    edit css header

--- src/Screen/HeaderDashboard.js
+++ src/Screen/HeaderDashboard.js
@@ -26,7 +26,7 @@ class HeaderDashboard extends Component{
         return(
             <div className="Hom">
                 <header className="Hom-header bg-light">
-                    <nav className="navbar navbar-light navbar-expand-sm navbar-toggleable-sm fixed-top bg-light">
+                    <nav className="navbar navbar-light navbar-expand-sm navbar-toggleable-sm fixed-top bg-light navbar-default">
                         <a style={{textAlign:"left"}}class="brand" href="/dashboard">DumbWays.id</a>
 
                         {/* <!-- Toggler/collapsibe Button --> */}
@@ -38,18 +38,16 @@ class HeaderDashboard extends Component{
                                 <li className="nav-item ml-auto" style={{marginRight:"10px"}}>
                                     <a href="/dashboard" className="nav-link">Home</a>
                                 </li>
-                                <li style={{marginRight:"10px"}} className="nav-item ml-auto">
+                                <li style={{marginRight:"10px"}} className="nav-item ml-auto ">
                                     <ul class="nav navbar-nav navbar-right">
-                                        {/* <i className="fa fa-bell" style={{fontsize:"24px"}}></i><Badge pill color="danger">9</Badge> */}
                                         <li class="dropdown">
-                                            {/* <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Notification (<b>2</b>)</a> */}
-                                            <i className="fa fa-bell " style={{fontsize:"24px"}}></i><Badge className="dropdown-toggle" data-toggle="dropdown" pill color="danger">9</Badge>                                        
-
+                                            <div className="dropdown-toggle" data-toggle="dropdown">
+                                                <i className="fa fa-bell " style={{fontsize:"24px"}}></i><Badge pill color="danger">9</Badge>                                        
+                                            </div>
                                             <ul class="dropdown-menu notify-drop">
                                                 <div class="notify-drop-title">
             	                                    <div class="row">
             		                                    <div class="col-md-6 col-sm-6 col-xs-6">
-                                                            {/* <i className="fa fa-bell" style={{fontsize:"24px"}}></i><Badge pill color="danger">9</Badge>                                         */}
                                                     		<div class="col-md-6 col-sm-6 col-xs-6">Unread (<b>9</b>)</div>
                                                         </div>
             		                                    <div class="col-md-6 col-sm-6 col-xs-6 text-right"><a href="" class="rIcon allRead" data-tooltip="tooltip" data-placement="bottom" title="Read All"><i class="fa fa-dot-circle-o"></i></a></div>
@@ -59,35 +57,37 @@ class HeaderDashboard extends Component{
                                                 <!-- notify content --> */}
                                                 <div class="drop-content">
                                                     <li>
-            		                                    <div class="col-md-3 col-sm-3 col-xs-3">
-                                                            <div class="notify-img">
-                                                                <img src="http://placehold.it/45x45" alt=""/>
-                                                            </div>
-                                                        </div>
-                                                        {/* <hr> */}
-            		                                    <p class="time">Şimdi</p>
-            		                                    {/* </hr> */}
-                                                    </li>
+                                                        <div class="col-md-3 col-sm-3 col-xs-3"><div class="notify-img"><img src="http://placehold.it/45x45" alt=""/></div></div>
+                                                        <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Ahmet</a> yorumladı. <a href="">Çicek bahçeleri...</a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
+                                                        
+                                                        <hr/>
+                                                        <p class="time">Şimdi</p>
+            		                                    </div>
+            	                                    </li>
                                                     <li>
-            		                                    <div class="col-md-3 col-sm-3 col-xs-3">
-                                                            <div class="notify-img">
-                                                                <img src="http://placehold.it/45x45" alt=""/>
-                                                            </div>
-                                                        </div>
-                                                        {/* <hr> */}
-            		                                    <p class="time">Şimdi</p>
-            		                                    {/* </hr> */}
-                                                    </li>
+            		                                    <div class="col-md-3 col-sm-3 col-xs-3"><div class="notify-img"><img src="http://placehold.it/45x45" alt=""/></div></div>
+            		                                    <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Ahmet</a> yorumladı. <a href="">Çicek bahçeleri...</a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
+                                                        <p>Lorem ipsum sit dolor amet consilium.</p>
+                                                        <p class="time">2 Hafta önce</p>
+            		                                    </div>
+            	                                    </li>
                                                     <li>
-            		                                    <div class="col-md-3 col-sm-3 col-xs-3">
-                                                            <div class="notify-img">
-                                                                <img src="http://placehold.it/45x45" alt=""/>
-                                                            </div>
-                                                        </div>
-                                                        {/* <hr> */}
-            		                                    <p class="time">Şimdi</p>
-            		                                    {/* </hr> */}
-                                                    </li>
+            		                                    <div class="col-md-3 col-sm-3 col-xs-3"><div class="notify-img"><img src="http://placehold.it/45x45" alt=""/></div></div>
+            		                                    <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Ahmet</a> yorumladı. <a href="">Çicek bahçeleri...</a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
+                                                        <p>Lorem ipsum sit dolor amet consilium.</p>
+                                                        <p class="time">2 Hafta önce</p>
+            		                                    </div>
+            	                                    </li>
+                                                    <li>
+            		                                    <div class="col-md-3 col-sm-3 col-xs-3"><div class="notify-img"><img src="http://placehold.it/45x45" alt=""/></div></div>
+            		                                    <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Ahmet</a> yorumladı. <a href="">Çicek bahçeleri...</a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
+                                                        <p>Lorem ipsum sit dolor amet consilium.</p>
+                                                        <p class="time">2 Hafta önce</p>
+            		                                    </div>
+            	                                    </li>
+                                                </div>
+                                                <div class="notify-drop-footer text-center">
+            	                                    <a href=""><i class="fa fa-eye"></i> Read All</a>
                                                 </div>
                                             </ul>
                                         </li>
