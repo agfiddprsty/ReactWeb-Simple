00dd3b89ce42adb303952fa577fbe6007423f89a
tsaqifsetyo <tsaqiftrooper@gmail.com>
2019-02-13 10:00:21 +0700

    fix footer attached on dashboard

--- src/Screen/ListVideo.js
+++ src/Screen/ListVideo.js
@@ -95,6 +95,9 @@ return (
                             </CardBody>
                         </a></Card>
                     </CardDeck>
+                    <br/>
+                    <br/>
+                    <br/>
                     
                     </div>
                 </div>
