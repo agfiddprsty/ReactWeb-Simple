097f171be5ee96615a2f4240533ee5d8d41ce801
Ramadhani Samudra Gawang Indiyanto <ganar.gatul@gmail.com>
2019-02-13 10:15:04 +0700

    fix notif drop

--- src/Screen/HeaderDashboard.js
+++ src/Screen/HeaderDashboard.js
@@ -47,8 +47,9 @@ class HeaderDashboard extends Component{
                                             <ul class="dropdown-menu notify-drop">
                                                 <div class="notify-drop-title">
             	                                    <div class="row">
-            		                                    <div class="col-md-6 col-sm-6 col-xs-6">
-                                                    		<div class="col-md-6 col-sm-6 col-xs-6">Unread (<b>9</b>)</div>
+            		                                    <div class="col-md-auto">
+                                                            {/* <div class="col-md-2 col-sm-2 col-xs-2">Unread (<b>9</b>)</div> */}
+                                                            Unread (<b>9</b>)
                                                         </div>
             		                                    <div class="col-md-6 col-sm-6 col-xs-6 text-right"><a href="" class="rIcon allRead" data-tooltip="tooltip" data-placement="bottom" title="Read All"><i class="fa fa-dot-circle-o"></i></a></div>
             	                                    </div>
@@ -56,31 +57,67 @@ class HeaderDashboard extends Component{
                                                 {/* <!-- end notify title -->
                                                 <!-- notify content --> */}
                                                 <div class="drop-content">
-                                                    <li>
-                                                        <div class="col-md-3 col-sm-3 col-xs-3"><div class="notify-img"><img src="http://placehold.it/45x45" alt=""/></div></div>
-                                                        <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Ahmet</a> yorumladı. <a href="">Çicek bahçeleri...</a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
+                                                    {/* <li>
+                                                        <div class="col-md-3">
+                                                            <div class="notify-img">
+                                                                <img src="http://placehold.it/45x45" alt=""/>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-auto">
+                                                            <a href="">Ahmet</a> yorumladı. 
+                                                            <a href="">Çicek bahçeleri...</a> 
+                                                            <a href="" class="rIcon">
+                                                                <i class="fa fa-dot-circle-o"></i>
+                                                            </a> */}
                                                         
-                                                        <hr/>
-                                                        <p class="time">Şimdi</p>
+                                                        {/* <hr/> */}
+                                                        {/* <p class="time">Şimdi</p>
             		                                    </div>
-            	                                    </li>
+                                                    </li> */}
                                                     <li>
-            		                                    <div class="col-md-3 col-sm-3 col-xs-3"><div class="notify-img"><img src="http://placehold.it/45x45" alt=""/></div></div>
-            		                                    <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Ahmet</a> yorumladı. <a href="">Çicek bahçeleri...</a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
+            		                                    {/* <div class="col-md-auto">
+                                                            <div class="notify-img">
+                                                                <img src="http://placehold.it/45x45" alt=""/>
+                                                            </div>
+                                                        </div> */}
+            		                                    <div class="col-md-auto">
+                                                        <a href="">Ahmet</a> yorumladı. 
+                                                        <a href="">Çicek bahçeleri...</a>
+                                                        <a href="" class="rIcon">
+                                                            <i class="fa fa-dot-circle-o"></i>
+                                                        </a>
                                                         <p>Lorem ipsum sit dolor amet consilium.</p>
                                                         <p class="time">2 Hafta önce</p>
             		                                    </div>
             	                                    </li>
                                                     <li>
-            		                                    <div class="col-md-3 col-sm-3 col-xs-3"><div class="notify-img"><img src="http://placehold.it/45x45" alt=""/></div></div>
-            		                                    <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Ahmet</a> yorumladı. <a href="">Çicek bahçeleri...</a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
+            		                                    {/* <div class="col-md-auto">
+                                                            <div class="notify-img">
+                                                                <img src="http://placehold.it/45x45" alt=""/>
+                                                            </div>
+                                                        </div> */}
+            		                                    <div class="col-md-auto">
+                                                        <a href="">Ahmet</a> yorumladı. 
+                                                        <a href="">Çicek bahçeleri...</a>
+                                                        <a href="" class="rIcon">
+                                                            <i class="fa fa-dot-circle-o"></i>
+                                                        </a>
                                                         <p>Lorem ipsum sit dolor amet consilium.</p>
                                                         <p class="time">2 Hafta önce</p>
             		                                    </div>
             	                                    </li>
                                                     <li>
-            		                                    <div class="col-md-3 col-sm-3 col-xs-3"><div class="notify-img"><img src="http://placehold.it/45x45" alt=""/></div></div>
-            		                                    <div class="col-md-9 col-sm-9 col-xs-9 pd-l0"><a href="">Ahmet</a> yorumladı. <a href="">Çicek bahçeleri...</a> <a href="" class="rIcon"><i class="fa fa-dot-circle-o"></i></a>
+            		                                    {/* <div class="col-md-auto">
+                                                            <div class="notify-img">
+                                                                <img src="http://placehold.it/45x45" alt=""/>
+                                                            </div>
+                                                        </div> */}
+            		                                    <div class="col-md-auto">
+                                                        <a href="">Ahmet</a> yorumladı. 
+                                                        <a href="">Çicek bahçeleri...</a>
+                                                        <a href="" class="rIcon">
+                                                            <i class="fa fa-dot-circle-o"></i>
+                                                        </a>
                                                         <p>Lorem ipsum sit dolor amet consilium.</p>
                                                         <p class="time">2 Hafta önce</p>
             		                                    </div>
