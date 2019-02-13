5158fcaa3f84f490c21d3d3e66c511b67821aa80
tsaqifsetyo <tsaqiftrooper@gmail.com>
2019-02-13 17:08:25 +0700

    update dashboard and add icon

--- src/Screen/ListVideo.js
+++ src/Screen/ListVideo.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardDeck,
     CardBody, Progress } from 'reactstrap';   
 import {withRouter,Link} from 'react-router-dom';
-import rnredux from './img/rnreduxplay.png';
+import rnredux from './img/rnredux.jpg';
 import './css/ListVideo.css';
 import {Input, InputGroupAddon, Button } from 'reactstrap';
 import { RenderLoop } from 'brace';
@@ -22,11 +22,16 @@ return (
                 <h3>Minggu 1</h3>
                     <CardDeck >
                         <Card className=" border-0">
-                            <a href='/detailvideo' className="modul"> 
+                            <a href='#' className="modul"> 
                             {/* <Link to = 'https://google.com/'></Link> */}
-                            <CardImg className="thumbnail-images" top width="10%" src={rnredux} alt="Card image cap" className="rounded imgcard"/>
+                            <CardImg class="zoom" top width="10%" src={rnredux} alt="Card image cap" class="image" className="rounded imgcard"/>
                             <CardBody className="crdbdy">
-                            <h5>1.Perkenalan</h5>
+                        <div class="overlay">       
+                             <a href="/detailvideo" class="icon">
+                                <i class="fa fa-play"></i>
+                            </a>
+                        </div>
+                            <h5>1. Perkenalan <span className="fa fa-clock-o" style={{fontSize:'15px',color:"red", marginLeft:"260px"}}> 04.32</span></h5>
                             <br></br>
                             </CardBody>
                             </a>
@@ -34,59 +39,84 @@ return (
                         <Card className=" border-0"><a href="/detailvideo" className="modul">
                             <CardImg top width="10%" src={rnredux} alt="Card image cap" className="rounded"/>
                             <CardBody className="crdbdy">
+                            <div class="overlay">
+                        <a href="/detailvideo" class="icon">
+                            <i class="fa fa-play"></i>
+                        </a>
 
-                            <h5>2.Instalasi</h5>
+                        </div>
+                            <h5>2. Instalasi <span className="fa fa-clock-o" style={{fontSize:'15px',color:"red", marginLeft:"270px"}}> 04.32</span> </h5>
                             <br></br>
                             </CardBody>
                         </a></Card>
                         <Card className=" border-0"><a href="/detailvideo" className="modul">
                             <CardImg top width="10%" src={rnredux} alt="Card image cap" className="rounded"/>
                             <CardBody className="crdbdy">
-                            <h5>3. Component</h5>
+                            <h5>3. Component <span className="fa fa-clock-o" style={{fontSize:'15px',color:"red",marginLeft:"260px"}}> 04.32</span> </h5>
+                            <br></br>
+                            </CardBody>
+                        </a></Card>
+                        <Card className=" border-0"><a href="/detailvideo" className="modul">
+                            <CardImg top width="10%" src={rnredux} alt="Card image cap" className="rounded"/>
+                            <CardBody className="crdbdy">
+
+                            <h5>4. Character <span className="fa fa-clock-o" style={{fontSize:'15px',color:"red", marginLeft:"260px"}}> 04.32</span> </h5>
                             <br></br>
                             </CardBody>
                         </a></Card>
                     </CardDeck>
                     <br/>
                     <br/>
-                <div className="col-md-12">
                     <CardDeck>
                         <Card className=" border-0"><a href="/detailvideo" className="modul">
-                            <CardImg className="thumbnail-images" style={{height:"255px"}} top width="100%" src={quis} alt="Card image cap" className="rounded imgcard"/>
+                            <CardImg top width="100%" src={rnredux} alt="Card image cap" className="rounded"/>
                             <CardBody className="crdbdy">
-                            <h5>4. Quiz</h5>
+                            <h5>4. Kuis Mingguan #1 <span className="fa fa-clock-o" style={{fontSize:'15px',color:"red", marginLeft:"260px"}}> 04.32</span> </h5>
                             <br></br>
                             </CardBody>
                             </a></Card>
                         <Card className="border-0"><a href="/detailvideo" className="modul">
                             <CardImg top width="100%" src={rnredux} alt="Card image cap" className="rounded"/>
                             <CardBody className="crdbdy">
-                            <h5>2. Instalasi</h5>
+                            <h5>2. Instalasi <span className="fa fa-clock-o" style={{fontSize:'15px',color:"red", marginLeft:"260px"}}> 04.32</span> </h5>
                             <br></br>
                             </CardBody>
                         </a></Card>
                         <Card className="border-0"><a href="/detailvideo" className="modul">
                             <CardImg top width="100%" src={rnredux} alt="Card image cap" className="rounded"/>
                             <CardBody className="crdbdy">
-                            <h5>3. Component</h5>
+                            <h5>3. Component <span className="fa fa-clock-o" style={{fontSize:'15px',color:"red", marginLeft:"260px"}}> 04.32</span> </h5>
+                            <br></br>
+                            </CardBody>
+                        </a></Card>
+                        <Card className=" border-0"><a href="/detailvideo" className="modul">
+                            <CardImg top width="10%" src={rnredux}  alt="Card image cap" className="rounded"/>
+                            <CardBody className="crdbdy">
+
+                            <h5>4. Character<span className="fa fa-clock-o" style={{fontSize:'15px',color:"red", marginLeft:"260px"}}> 04.32</span> </h5>
                             <br></br>
                             </CardBody>
                         </a></Card>
                     </CardDeck>
                     <br/>
                     <br/>
+                    <div style={{borderWidth:"100px",borderBottom:"solid",width:"1470px",borderColor:"#e7e7e7"}}></div>
+                    <br/>
                      <h3> Minggu 2</h3> 
+                     <span>Konten Terkunci,selesaikan dulu semua kuis di Minggu 1</span>
                     <CardDeck>
                         <Card className="border-0"><a href="#" className="modul">
                             <CardImg top width="100%" src={rnredux} style={{filter: "grayscale(100%)"}} alt="Card image cap" className="rounded"/>
                             <CardBody className="crdbdy">
-                            <h5>1. Perkenalan</h5>
+                            <i class="fa fa-key"></i>
+                            <h5>1. Perkenalan</h5>  
                             <br></br>
                             </CardBody>
                         </a></Card>
                         <Card className="border-0"><a href="#" className="modul">
                             <CardImg top width="100%" src={rnredux} style={{filter: "grayscale(100%)"}} alt="Card image cap" className="rounded"/>
                             <CardBody className="crdbdy">
+                            <i class="fa fa-key"></i>
                             <h5>2. Instalasi</h5>
                             <br></br>
                             </CardBody>
@@ -94,7 +124,50 @@ return (
                         <Card className="border-0"><a href="#" className="modul">
                             <CardImg top width="100%" src={rnredux} style={{filter: "grayscale(100%)"}} alt="Card image cap" className="rounded"/>
                             <CardBody className="crdbdy">
+                            <i class="fa fa-key"></i>
                             <h5>3. Component</h5>
+                            <br></br>
+                            </CardBody>
+                        </a></Card>
+                        <Card className=" border-0"><a href="/detailvideo" className="modul">
+                            <CardImg top width="10%" src={rnredux}  style={{filter: "grayscale(100%)"}} alt="Card image cap" className="rounded"/>
+                            <CardBody className="crdbdy">
+                            <i class="fa fa-key"></i>
+                            <h5>4. Character</h5>
+                            <br></br>
+                            </CardBody>
+                        </a></Card>
+                    </CardDeck>
+                    <CardDeck>
+                        <Card className="border-0"><a href="#" className="modul">
+                            <CardImg top width="100%" src={rnredux} style={{filter: "grayscale(100%)"}} alt="Card image cap" className="rounded"/>
+                            <CardBody className="crdbdy">
+                            <i class="fa fa-key"></i>
+                            <h5>1. Kuis Mingguan #2</h5>  
+                            <br></br>
+                            </CardBody>
+                        </a></Card>
+                        <Card className="border-0"><a href="#" className="modul">
+                            <CardImg top width="100%" src={rnredux} style={{filter: "grayscale(100%)"}} alt="Card image cap" className="rounded"/>
+                            <CardBody className="crdbdy">
+                            <i class="fa fa-key"></i>
+                            <h5>2. Instalasi</h5>
+                            <br></br>
+                            </CardBody>
+                        </a></Card>
+                        <Card className="border-0"><a href="#" className="modul">
+                            <CardImg top width="100%" src={rnredux} style={{filter: "grayscale(100%)"}} alt="Card image cap" className="rounded"/>
+                            <CardBody className="crdbdy">
+                            <i class="fa fa-key"></i>
+                            <h5>3. Component</h5>
+                            <br></br>
+                            </CardBody>
+                        </a></Card>
+                        <Card className=" border-0"><a href="/detailvideo" className="modul">
+                            <CardImg top width="10%" src={rnredux}  style={{filter: "grayscale(100%)"}} alt="Card image cap" className="rounded"/>
+                            <CardBody className="crdbdy">
+                            <i class="fa fa-key"></i>
+                            <h5>4. Character</h5>
                             <br></br>
                             </CardBody>
                         </a></Card>
@@ -102,9 +175,7 @@ return (
                     <br/>
                     <br/>
                     <br/>
-                    
                     </div>
-                </div>
                 </div>
         );
     }
