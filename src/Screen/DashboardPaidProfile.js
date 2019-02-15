bdc63c2726d81d6932c5d0397d7e66b8b4c7cce4
Ardhana <mardhanawn@gmail.com>
2019-02-15 19:05:36 +0700

    change input & button

--- src/Screen/DashboardPaidProfile.js
+++ src/Screen/DashboardPaidProfile.js
@@ -9,16 +9,16 @@ class DashboardPaidProfile extends Component {
                 <div className="text-center" style={{padding:'145px 0 0 0'}}>
                     <h1 style={{color:'##3B3B3B', fontWeight:'510', fontFamily:'Circular Std Black, sans-serif;', fontSize:'2.17000000em',display:'block', margin:'-5px 0 0 0',}}>
                     Terimakasih telah mengikuti bootcamp online ini </h1>
-                    <p style={{color:'#666666', fontWeight:'400', fontFamily:'Open Sans, helvetica, Arial, sans serif', fontSize:'1.39000000em', display:'block', margin:'15px 0 39px 0'}}>
+                    <p style={{color:'grey', fontWeight:'400', fontFamily:'Open Sans, helvetica, Arial, sans serif', fontSize:'1.39000000em', display:'block', margin:'15px 0 39px 0'}}>
                     Silahkan melengkapi profile anda demi kemudahan kami dalam menyalurkan kerja dan administrasi
                     </p>
                 </div>
             </div>
 
             <div style={{paddingBottom:'13px'}}>
-                <div class="daftar" style={{boxShadow:'0 0 7px 0 rgba(136, 136, 136, 0.4)', width:'37%', backgroundColor:'#FFF', position:'center', margin:'25px 0 100px 430px', border:'1px solid #ececec'}}> 
-                    <div class="col-md-12" style={{margin:'0 25px 0 75px'}}>
-                    <form class="login-form validate-form" style={{margin:'50px 25px 0 25px'}}>
+                <div class="daftar" style={{boxShadow:'0 0 20px 1px rgba(136, 136, 136, 0.4)', width:'37%', backgroundColor:'#FFF', position:'center', margin:'25px 0 100px 430px', border:'1px solid #ececec'}}> 
+                    <div class="col-md-12" style={{margin:'0 25px 0 10px'}}>
+                    <form class="login-form validate-form" style={{width:'91%', margin:'25px 15px 0 15px'}}>
 
                         <div class="wrap-input validate-input" data-validate = "">
                             <input class="input" type="email" name="notelp" placeholder="Nomor Telepon"/>
@@ -54,7 +54,7 @@ class DashboardPaidProfile extends Component {
                     
                     </form>
 
-                            <div className="text-center btn-lg" style={{width:'95%', padding:'15px 0 15px 167px'}}>
+                            <div className="d-flex justify-content-end text-center btn-lg" style={{width:'95%', padding:'15px 0 15px 167px'}}>
                                 <a href="/dashboard">
                                     <button type="button text-center" class="btn btn-danger btn-lg" style={{padding:'15px 34px 20px 34px', borderRadius:'0', boxShadow:'0 0 15px 0 rgba(136, 136, 136, 0.4)'}}>Selesai</button>
                                 </a>
