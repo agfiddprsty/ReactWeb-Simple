275ed8bf30c5ba4b5a640b087670a094b363aad1
Agfid Danu Prasetyo <3103117235@student.smktelkom-pwt.sch.id>
2019-02-21 17:17:07 +0700

    add radio button

--- src/Screen/DashboardPaidProfile.js
+++ src/Screen/DashboardPaidProfile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import LabelFloatInput from 'label-float-input';
-import Header from './HeaderPage'
+import Header from './HeaderPage';
 import './css/DashboardPaidProfile.css';
 
 class DashboardPaidProfile extends Component {
@@ -9,6 +9,7 @@ class DashboardPaidProfile extends Component {
         this.finishhandle = this.finishhandle.bind(this);
         this.pendidikanhandle = this.finishhandle.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleChecked = this.handleChecked.bind(this);
         this.moveLogin = this.moveLogin.bind(this);
         this.state = {
             isverified: false,
@@ -18,7 +19,9 @@ class DashboardPaidProfile extends Component {
             adress:'',
             summary:'',
             emailValid: true,
-            ressjson:''
+            ressjson:'',
+            checked: false,
+            selectedValue: ''
 
         }
     }
@@ -62,6 +65,17 @@ class DashboardPaidProfile extends Component {
             console.log(this.state.selectedValue);
         }        
         
+    }
+
+    handleChecked(){
+        this.setState({
+            checked: true
+        })
+    }
+
+    handleValue(value) {
+        this.setState({selectedValue: value});
+        console.log(this.state.selectedValue);
     }
 
     pendidikanhandle(){
@@ -177,29 +191,31 @@ class DashboardPaidProfile extends Component {
                                             <input type="text" name="adress" onChange={this.handleChange} value={this.state.adress} required="" style={{fontFamily:'Open Sans, helvetica, Arial, sans serif'}}/>
                                             <label style={{fontFamily:'Open Sans, helvetica, Arial, sans serif'}}>Alamat</label>
                                         </div>
-                                        <div style={{padding:'20px 0px 20px 20px'}}>
-                                            <form>
-                                                <label class="radio-inline" style={{paddingRight:'20px'}}>
-                                                    <input type="radio" name="optradio" checked/>Male
-                                                </label>
-                                                <label class="radio-inline">
-                                                    <input type="radio" name="optradio"/>Female
-                                                </label>
+                                        <div>
+                                            <form className="text-left" style={{marginLeft: '20px'}}>
+                                                <div class="custom-control custom-radio custom-control-inline" style={{paddingRight: '100px'}}>
+                                                    <input type="radio" onChange={this.handleValue} selectedValue={this.state.selectedValue} class="custom-control-input" id="lakilaki" name="gender" value="Laki-Laki"/>
+                                                    <label class="custom-control-label" for="lakilaki">Laki-Laki</label>
+                                                </div>
+                                                <div class="custom-control custom-radio custom-control-inline">
+                                                    <input type="radio" onChange={this.handleValue} selectedValue={this.state.selectedValue} class="custom-control-input" id="perempuan" name="gender" value="Perempuan"/>
+                                                    <label class="custom-control-label" for="perempuan">Perempuan</label>
+                                                </div> 
                                             </form>
                                         </div>
                                         <div>
-                                            <form class="row">
-                                                <div class="form-group col-1">
-                                                    <label for="Date">Date</label>
-                                                    <select class="form-control" id="exampleFormControlSelect1">
-                                                    <option>01</option><option>02</option><option>03</option><option>04</option><option>05</option><option>06</option><option>07</option><option>08</option><option>09</option><option>10</option><option>11</option><option>12</option><option>13</option><option>14</option><option>15</option><option>16</option><option>17</option><option>18</option><option>19</option><option>20</option>
+                                            <form class="row" style={{marginLeft: '0'}}>
+                                                <div class="form-group col-2">
+                                                    <label for="Date" style={{paddingBottom:'100px'}}>Date</label>
+                                                    <select class="form-control text-center" id="exampleFormControlSelect1">
+                                                        <option>1</option><option>2</option><option>3</option><option>4</option><option>5</option><option>6</option><option>7</option><option>8</option><option>9</option><option>10</option><option>11</option><option>12</option><option>13</option><option>14</option><option>15</option><option>16</option><option>17</option><option>18</option><option>19</option><option>20</option>
                                                     </select>
                                                 </div>
-                                                <div class="form-group col-1">
+                                                <div class="form-group col-2">
                                                     <label for="Date">Month</label>
-                                                    <select class="form-control" id="exampleFormControlSelect1">
-                                                    <option>01</option><option>02</option><option>03</option><option>04</option><option>05</option><option>06</option>
-                                                    <option>07</option><option>08</option><option>039</option><option>10</option><option>11</option><option>12</option>
+                                                    <select class="form-control text-center" id="exampleFormControlSelect1">
+                                                        <option>1</option><option>2</option><option>3</option><option>4</option><option>5</option><option>6</option>
+                                                        <option>7</option><option>8</option><option>9</option><option>10</option><option>11</option><option>12</option>
                                                     </select>
                                                 </div>
                                             </form>
