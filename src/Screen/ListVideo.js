6c89ff43643220e9119ded42d04f3908a7ab0656
tsaqifsetyo <tsaqiftrooper@gmail.com>
2019-02-25 18:54:41 +0700

    fix header dashboard

--- src/Screen/ListVideo.js
+++ src/Screen/ListVideo.js
@@ -7,7 +7,7 @@ class ListVideo extends Component {
 
 render() {
 return (
-        <div style={{marginLeft:"-50px",overflow:"hidden",backgroundColor:"#fff"}} >
+        <div style={{marginLeft:"-25px",overflow:"hidden",backgroundColor:"#fff"}} >
         <div class="container">
             <div>
                 <br/>
