4f18e67d7445eabf56aae88aef4fa2b8d181e680
Agfid Danu Prasetyo <3103117235@student.smktelkom-pwt.sch.id>
2019-02-25 17:05:39 +0700

    add dropdown at image header fix css payment fix css dashboard unpaid

--- src/Screen/HeaderPage.js
+++ src/Screen/HeaderPage.js
@@ -10,12 +10,18 @@ class Header extends Component {
           <div className="headsilab fixed-top bg-light">
             <header className="Hom-header bg-light">
                 <nav className="container navbar navbar-light navbar-expand-sm navbar-toggleable-sm fixed-top bg-light" style={{height: '80px'}}>
-                  <a class="brand" href="#">
+                  <a class="brand" href="/home">
                   <img alt="" className="imgbran" src="https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=height:60/https://www.filepicker.io/api/file/mWcP190oTCqiAo5VjrE8"/>
                   </a> 
                   <ul class="navbar-nav ml-auto">
-                    <li className="nav-item" style={{marginLeft: '50px', width: '50px'}}>
-                      <img src={boy} alt="" className="rounded-circle img-fluid" style={{backgroundColor: '#fff', width: '50px', maxWidth: '100%', marginLeft:"100px"}}/>
+                    <li className="nav-item dropdown" style={{marginLeft: '50px', width: '50px'}}>
+                      <a class="nav-link togldrpdn" href="#" id="navbardrop" data-toggle="dropdown">
+                        <img src={boy} alt="" className="rounded-circle img-fluid imgboy"/>
+                      </a>
+                      <div class="dropdown-menu">
+                        <a class="dropdown-item" href="/profilefilled">Profile</a>
+                        <a class="dropdown-item" href="/register">Logout</a>
+                      </div>
                     </li>
                   </ul>
                 </nav>
--- src/Screen/Payment.js
+++ src/Screen/Payment.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import {withRouter,Link} from 'react-router-dom';
-import { red } from '@material-ui/core/colors';
 import bca from './img/bca.png';
 import './css/payment.css';
 import mandiri from './img/bankmandiri.jpg';
@@ -16,25 +14,23 @@ class Payment extends Component {
                     <Header/>
                 </div>
                 <div className="kontener">
-                    <section className="kotak">
+                    <section className="sqare">
                         <h2 className="butuh">Pembayaran pendaftaran dibutuhkan</h2>
                         <span className="silahkan"> Silahkan membayar biaya  pendaftaran online bootcamp sebesar IDR <br/> 500.000 untuk dapat mengakses konten kami</span>
-                        <div style={{marginBottom: '30px'}}>
+                        <div className="norek">
                             <img className="rounded icobca" src={bca} alt="" /> 
-                            <span className="bca" style={{fontWeight: '600', fontFamily: 'helvetica, Arial, sans serif'}}> 320 0401 823 a/n Ega Wachid</span>
+                            <span className="bca"> 320 0401 823 a/n Ega Wachid</span>
                             <br></br>
                             <img className="rounded icoman" src={mandiri} alt=""/> 
-                            <span className="mandiri" style={{marginBottom: '50px', fontWeight: '600', fontFamily: 'helvetica, Arial, sans serif'}}> 90000 2773 5407 a/n Ega Wachid</span>
+                            <span className="mandiri"> 90000 2773 5407 a/n Ega Wachid</span>
                         </div>
                     </section>
                     <div className="text-center">
-                        <h6 style={{
-                            height:'auto',color:'black', marginTop: '50px', marginLeft: '20px', fontFamily: 'Open Sans, helvetica, Arial, sans serif'
-                        }}>Silahkan Konfirmasi, jika sudah melakukan pembayaran, melalui:</h6>
+                        <h6 className="plsconfrm">Silahkan Konfirmasi, jika sudah melakukan pembayaran, melalui:</h6>
                         <div>
                             <img src={telegram} alt="" className="icotel"/> 
                             <img src={whatsapp} alt="" className="icowa"/> 
-                            <span style={{marginLeft: '10px', fontFamily: 'Open Sans, helvetica, Arial, sans serif'}}>  +62 8564 1278 479</span>
+                            <span className="nophone">  +62 8564 1278 479</span>
                             <br/>
                             <br/>
                         </div>
