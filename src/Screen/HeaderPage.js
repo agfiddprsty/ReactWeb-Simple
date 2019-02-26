2b3558a326a14b23129d7813f8b03e4c7b2b1f2d
Agfid Danu Prasetyo <3103117235@student.smktelkom-pwt.sch.id>
2019-02-26 17:56:02 +0700

    update landing page update header page update footer landing page

--- src/Screen/HeaderPage.js
+++ src/Screen/HeaderPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './css/Header.css';
+import  dwicon from './img/Logodw.png'
 import boy from './img/man1.png';
 
 class Header extends Component {
@@ -9,12 +10,12 @@ class Header extends Component {
         <div className="d-none d-lg-block d-xl-block">
           <div className="headsilab fixed-top bg-light">
             <header className="Hom-header bg-light">
-                <nav className="container navbar navbar-light navbar-expand-sm navbar-toggleable-sm fixed-top bg-light" style={{height: '80px'}}>
+                <nav className="container navbar navbar-light navbar-expand-sm navbar-toggleable-sm fixed-top bg-light" style={{height: '75px'}}>
                   <a class="brand" href="/home">
-                  <img alt="" className="imgbran" src="https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=height:60/https://www.filepicker.io/api/file/mWcP190oTCqiAo5VjrE8"/>
+                    <img alt="" className="imgbran" src={dwicon}/>
                   </a> 
                   <ul class="navbar-nav ml-auto">
-                    <li className="nav-item dropdown" style={{marginLeft: '50px', width: '50px'}}>
+                    <li className="nav-item dropdown" style={{marginLeft: '200px', width: '75px'}}>
                       <a class="nav-link togldrpdn" href="#" id="navbardrop" data-toggle="dropdown">
                         <img src={boy} alt="" className="rounded-circle img-fluid imgboy"/>
                       </a>
@@ -37,7 +38,27 @@ class Header extends Component {
                   </a> 
                   <ul class="navbar-nav ml-auto">
                     <li className="nav-item" style={{marginLeft: '50px', width: '50px'}}>
-                      <img src={boy} alt="" className="rounded-circle img-fluid ml-auto" style={{backgroundColor: '#fff', width: '50px', maxWidth: '100%'}}/>
+                      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+                      <div class="collapse navbar-collapse bg-light colap" id="collapsibleNavbar" >
+                        <ul class="navbar-nav ml-auto">
+                          <li className="nav-item">
+                            <a href='/profilefilled' className="nav-link">Masuk</a>
+                          </li>
+                          <li className="nav-item">
+                            <a href="/register" className="nav-link active">Logout</a>
+                          </li>
+                        </ul>
+                      </div>
+                      {/* <a class="nav-link" href="#" id="navbardrop" data-toggle="dropdown">
+                        <img src={boy} alt="" className="rounded-circle img-fluid"/>
+                      </a>
+                      <div class="dropdown-menu">
+                        <a class="dropdown-item" href="/profilefilled">Profile</a>
+                        <a class="dropdown-item" href="/register">Logout</a>
+                      </div> */}
+                      {/* <img src={boy} alt="" className="rounded-circle img-fluid ml-auto" style={{backgroundColor: '#fff', width: '50px', maxWidth: '100%'}}/> */}
                     </li>
                   </ul>
                 </nav>
--- src/Screen/footerLanding.js
+++ src/Screen/footerLanding.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './css/footerLanding.css';
+import dwhite from './img/dwhite.png';
 
 class Footer extends Component {
   render() {
@@ -9,34 +10,34 @@ class Footer extends Component {
           <div>
             <div className="row" style={{borderBottom: '1px solid #ebebeb', paddingBottom: '50px'}}>
                 <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6" style={{paddingBottom: '20px'}}>
-                    <img alt="" src="https://image.ibb.co/eBLRvU/logo_footer.png"/>
-                    <div>
-                        <p className="pertama">Permata Bintaro, South Tangerang</p>
-                        <p className="duaaa">+62 8564 1278 479</p>
-                        <p className="duaaa">radiegtya@dumbways.id</p>
+                    <img alt="" src={dwhite} className="dwhite"/>
+                    <div className="contact">
+                        <p className="addrs">Permata Bintaro, South Tangerang</p>
+                        <p className="nope">+62 8564 1278 479</p>
+                        <p className="email">radiegtya@dumbways.id</p>
                     </div>
                 </div>
                 <div className="col-lg col-md col-sm col-xs" style={{paddingBottom: '20px'}}>
-                    <strong>LEARN</strong>
+                    <strong className="titlefoot">LEARN</strong>
                     <p className="pertama">About</p>
                     <p className="duaaa">Browse</p>
                     <p className="duaaa">Sign In</p>
                 </div>
                 <div className="col-lg col-md col-sm col-xs" style={{paddingBottom: '20px'}}>
-                    <strong>DISCUSS</strong>
+                    <strong className="titlefoot">DISCUSS</strong>
                     <p className="pertama">Forum</p>
                     <p className="duaaa">Podcast</p>
                     <p className="duaaa">Sign In</p>
                 </div>
                 <div className="col-lg col-md col-sm col-xs" style={{paddingBottom: '20px'}}>
-                    <strong>EXTRAS</strong>
+                    <strong className="titlefoot">EXTRAS</strong>
                     <p className="pertama">Testimonials</p>
                     <p className="duaaa">FAQ</p>
                     <p className="duaaa">Privacy & Terms</p>
                 </div>
             </div>
             <div className="text-center copiret">
-                <p>&copy; DumbWays 2019 - All rights Reserved. Yes, all of them. That means you. Ega.</p>
+                <p className="txtcpy">&copy; DumbWays 2019 - All rights Reserved. Yes, all of them. That means you. Ega.</p>
             </div>
           </div>
         </footer>
