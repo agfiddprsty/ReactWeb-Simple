08a793645ec906b8ee71274d6bf6714cde1c6bcd
Agfid Danu Prasetyo <3103117235@student.smktelkom-pwt.sch.id>
2019-03-01 15:24:31 +0700

    change BoD

--- src/Screen/ProfileFill.js
+++ src/Screen/ProfileFill.js
@@ -664,18 +664,6 @@ class ProfileFill extends Component {
                                                                                 </p>
                                                                             </div>
                                                                         </div>
-                                                                        <div class="row drpdndt">
-                                                                            <select name="month" class="col-1 custom-select mb-3 optdate">
-                                                                                <option selected>01</option><option>02</option><option>03</option><option>04</option><option>05</option>
-                                                                                <option>06</option><option>07</option><option>08</option><option>09</option><option>10</option>
-                                                                                <option>11</option><option>12</option>
-                                                                            </select>
-
-                                                                            <select name="year" class="col-1 custom-select mb-3 optdate">
-                                                                                <option selected>2000</option><option>1999</option><option>1998</option><option>1997</option><option>1996</option>
-                                                                                <option>1995</option><option>1994</option><option>1993</option><option>1992</option><option>1991</option><option>1990</option>
-                                                                            </select>
-                                                                        </div>
                                                                         <div class="row">
                                                                             <div class="col-sm-3 col-md-1">
                                                                                 <p class="until">
