251d6f4826eaba22f21eccd3a75bf9040871b1f4
Ramadhani Samudra Gawang Indiyanto <ganar.gatul@gmail.com>
2019-03-05 17:41:03 +0700

    revvvvv detailquiz and video

--- src/Screen/Quiz.js
+++ src/Screen/Quiz.js
@@ -53,9 +53,9 @@ class Quiz extends Component{
                             <ul class="nav flex-column nav-pills qz-ul-gd qz-ul-cl" id="tabbb" style={{borderRadius:"0px!important"}}>
                                 <li class="nav-item pill" >
                                     <table>
-                                        <tr className={"nav-link qz-tb-gd qz-tb-cl rounded-0 "+ (this.state.aclick)}>
+                                        <tr className={"nav-link qz-tb-gd qz-tb-cl rounded-0 "+ (this.state.aclick)} style={{marginTop:"10px"}}>
                                             <td>
-                                                <input type="checkbox" className="nav-link" style={{marginTop:"10px",marginRight:"10px"}} />
+                                                <input type="checkbox" className="nav-link" style={{marginTop:"3px",marginRight:"10px"}} />
                                             </td>
                                             <td>
                                                 <a  class="aquiz" href="#vtab1" data-toggle="tab" onClick={this.onaCLick} >
@@ -71,49 +71,48 @@ class Quiz extends Component{
                                             </td>
                                         </tr>
                                     </table>
-                                    
                                 </li>
                                 <li class="nav-item pill">
                                     <table>
-                                            <tr className={"nav-link qz-tb-gd qz-tb-cl rounded-0 "+ (this.state.bclick)}>
-                                                <td>
-                                                    <input type="checkbox" className="nav-link" style={{marginTop:"10px",marginRight:"10px"}} />
-                                                </td>
-                                                <td>
-                                                    <a  class="aquiz" href="#vtab2" data-toggle="tab" onClick={this.onbCLick} >
+                                        <tr className={"nav-link qz-tb-gd qz-tb-cl rounded-0 "+ (this.state.bclick)}>
+                                            <td>
+                                                <input type="checkbox" className="nav-link" style={{marginTop:"3px",marginRight:"10px"}} />
+                                            </td>
+                                            <td>
+                                                <a  class="aquiz" href="#vtab2" data-toggle="tab" onClick={this.onbCLick} >
+                                                    <span>
+                                                    <a class="aquiz" href="#vtab2" data-toggle="tab" onClick={this.onbCLick} style={{marginTop:"10px"}}>
+                                                    Membuat Component Dengan Gelap
                                                         <span>
-                                                        <a class="aquiz" href="#vtab2" data-toggle="tab" onClick={this.onbCLick} style={{marginTop:"10px"}}>
-                                                        Membuat Component Dengan Gelap
-                                                            <span>
-                                                                <i className="fa fa-check-circle" style={{marginLeft:"20px",borderRadius:"10px",color:"#4CAF50"}}>  </i>
-                                                            </span>
-                                                        </a>
+                                                            <i className="fa fa-check-circle" style={{marginLeft:"20px",borderRadius:"10px",color:"#4CAF50"}}>  </i>
                                                         </span>
                                                     </a>
-                                                </td>
-                                            </tr>
+                                                    </span>
+                                                </a>
+                                            </td>
+                                        </tr>
                                     </table>                
                                 </li>
                                 <li class="nav-item pill">
                                     <table>
-                                            <tr className={"nav-link qz-tb-gd qz-tb-cl rounded-0 "+ (this.state.cclick)}>
-                                                <td>
-                                                    <input type="checkbox" className="nav-link" style={{marginTop:"10px",marginRight:"10px"}} />
-                                                </td>
-                                                <td>
-                                                    <a  class="aquiz" href="#vtab3" data-toggle="tab" onClick={this.oncCLick} >
+                                        <tr className={"nav-link qz-tb-gd qz-tb-cl rounded-0 "+ (this.state.cclick)}>
+                                            <td>
+                                                <input type="checkbox" className="nav-link" style={{marginTop:"3px",marginRight:"10px"}} />
+                                            </td>
+                                            <td>
+                                                <a  class="aquiz" href="#vtab3" data-toggle="tab" onClick={this.oncCLick} >
+                                                    <span>
+                                                    <a class="aquiz" href="#vtab3" data-toggle="tab" onClick={this.oncCLick} style={{marginTop:"10px"}}>
+                                                    Membuat Component Dengan Gelap
                                                         <span>
-                                                        <a class="aquiz" href="#vtab3" data-toggle="tab" onClick={this.oncCLick} style={{marginTop:"10px"}}>
-                                                        Membuat Component Dengan Gelap
-                                                            <span>
-                                                                <i className="fa fa-times-circle" style={{marginLeft:"20px",borderRadius:"10px",color:"#E44E41"}}>  </i>
+                                                            <i className="fa fa-times-circle" style={{marginLeft:"20px",borderRadius:"10px",color:"#E44E41"}}>  </i>
 
-                                                            </span>
-                                                        </a>
                                                         </span>
                                                     </a>
-                                                </td>
-                                            </tr>
+                                                    </span>
+                                                </a>
+                                            </td>
+                                        </tr>
                                     </table>
                                 </li>   
                                
@@ -125,7 +124,12 @@ class Quiz extends Component{
                                     <h5>Membuat Component Dengan Gelap</h5>
                                     <p>Place a link with the class <code>.nav-tabs-dropdown.btn.btn-block.btn-primary</code> above the tabs list and add an id of <code>#nav-tabs-wrapper</code> to the tabs list for the dropdown to work.</p>
                                     <div style={{backgroundColor:"#4CAF50",width:"300px",height:"40px"}}>
-                                        <i className="fa fa-info-circle" style={{marginLeft:"10px",marginTop:"10px",color:"#FFF"}}> Jawaban Benar</i>
+                                        <i className="fa fa-info-circle" style={{marginLeft:"10px",marginTop:"10px",color:"#FFF"}}> 
+                                        <span style={{marginLeft:"10px"}}>
+                                        Jawaban Benar
+
+                                        </span>
+                                        </i>
                                     </div>
                                     <Disscus/>
                                 </div>
@@ -133,7 +137,12 @@ class Quiz extends Component{
                                     <h5>Membuat Component Dengan Gelap</h5>
                                     <p> Mauris imperdiet dignissim ante, in efficitur mauris elementum sed. Cras vulputate malesuada magna. Morbi tincidunt eros a dui cursus, vitae dignissim nulla scelerisque. Duis pharetra scelerisque mi vel luctus. Cras eu purus efficitur, blandit nisi id, fringilla nulla.</p>
                                     <div style={{backgroundColor:"#4CAF50",width:"300px",height:"40px"}}>
-                                        <i className="fa fa-info-circle" style={{marginLeft:"10px",marginTop:"10px",color:"#FFF"}}> Jawaban Benar</i>
+                                        <i className="fa fa-info-circle" style={{marginLeft:"10px",marginTop:"10px",color:"#FFF",marginRight:"10px"}}>
+                                        <span style={{marginLeft:"10px"}}>
+                                        Jawaban Benar
+
+                                        </span>
+                                        </i>
                                     </div>
                                     <Disscus/>
                                 </div>
@@ -141,7 +150,12 @@ class Quiz extends Component{
                                     <h5>Membuat Component dengan Terang</h5>
                                     <p>Etiam id pharetra quam. Morbi tristique nunc vel sapien dapibus, sit amet imperdiet quam venenatis. Vestibulum et suscipit urna. Suspendisse volutpat quis est eu volutpat. Nulla non tortor venenatis turpis congue aliquet. Vivamus at elit vel massa elementum tempor sit amet sed odio. Nullam placerat, arcu sed ullamcorper ornare, erat erat placerat quam, in feugiat nulla purus in nunc. Maecenas vitae erat auctor, aliquam tellus et, vulputate eros.</p>
                                     <div style={{backgroundColor:"#FF4344",width:"300px",height:"40px"}}>
-                                        <i className="fa fa-info-circle" style={{marginLeft:"10px",marginTop:"10px",color:"#FFF"}}> Jawaban Salah, Mohon Koreksi</i>
+                                        <i className="fa fa-info-circle" style={{marginLeft:"10px",marginTop:"10px",color:"#FFF",marginRight:"10px"}}>
+                                         <span style={{marginLeft:"10px"}}>
+                                         Jawaban Salah, Mohon Koreksi
+
+                                        </span>
+                                         </i>
                                     </div>
                                     <Disscus/>
                                 </div>
