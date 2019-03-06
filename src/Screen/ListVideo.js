6bae1a9658160d8e6b51a34fa1e9a01b355c41de
tsaqifsetyo <tsaqiftrooper@gmail.com>
2019-03-06 12:40:44 +0700

    add array state mapping on listvideo

--- src/Screen/ListVideo.js
+++ src/Screen/ListVideo.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import './css/ListVideo.css';
 import ply from './img/ply.png';
-// import { Link } from 'react-router-dom';
 import lvl from './img/Level.png';
 import lock from './img/LockRed.png'
 class ListVideo extends Component {
     constructor(){
         super()
         this.state={
-            urlbackground:"https://source.unsplash.com/Mv4sg6_6qmE/650x240",
-            url:"linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(https://source.unsplash.com/95YRwf6CNw8/450x250)",
             listvideo:[
-                {id:"1",image:"https://source.unsplash.com/Mv4sg6_6qmE/650x240",tittle:"Test"},
-                {id:"2",image:"https://source.unsplash.com/Mv4sg6_6qmE/650x240",tittle:"Test"},
-                {id:"3",image:"https://source.unsplash.com/Mv4sg6_6qmE/650x240",tittle:"Test"},
-                {id:"4",image:"https://source.unsplash.com/Mv4sg6_6qmE/650x240",tittle:"Test"},
-            ],
+                {id:"1",image:"https://source.unsplash.com/Mv4sg6_6qmE/650x240",tittle:"1. Perkenalan",type:"video"},
+                {id:"2",image:"https://source.unsplash.com/F2WX4BTIkpQ/650x240",tittle:"2. Komponen",type:"video"},
+                {id:"3",image:"https://source.unsplash.com/Mv4sg6_6qmE/650x240",tittle:"3. Instalation",type:"video"},
+                {id:"4",image:"https://source.unsplash.com/F2WX4BTIkpQ/650x240",tittle:"4. Character",type:"video"},
+                {id:"5",image:"https://source.unsplash.com/ZSPBhokqDMc/550x240",tittle:"Quiz Minggu 1",type:"quiz"},
+                {id:"6",image:"https://source.unsplash.com/Mv4sg6_6qmE/650x240",tittle:"1. Perkenalan",type:"video"},
+                {id:"7",image:"https://source.unsplash.com/F2WX4BTIkpQ/650x240",tittle:"2. Komponen",type:"video"},
+                {id:"8",image:"https://source.unsplash.com/Mv4sg6_6qmE/650x240",tittle:"3. Instalation",type:"video"},
+
+            ],  
         }
     }
     componentDidMount(){
@@ -31,105 +33,49 @@ return (
                 <br/>
             </div>
             <div class="row text-center text-lg-left">
-            <div class="col-lg-3 col-md-4 col-6">
-                <div className="d-flex justify-content-center listA">
-                    <a href="/video/example" class="d-block mb-4 h-100">
-                        <img class="img-fluid img-thumbnail plybtn" src={ply} alt=""></img>
-                        <br/>
-                    </a>
-                </div>
-                <a style={{paddingTop:"15px"}} href="/video/example" class="d-block mb-4 h-100">
-                    <span className="span-title-video" style={{fontFamily:"Montserrat",fontWeight:"500"}}>1. Perkenalan</span>
-                    <span className="span-time" style={{fontSize:"13px"}}><span className="far fa-clock" style={{opacity:0.4,marginLeft:"96px",color:"red"}}></span> 04:32 </span>
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-4 col-6">
-                <div className="d-flex justify-content-center listB">
-                    <a href="/video/example" class="d-block mb-4 h-100">
-                    <img class="img-fluid img-thumbnail plybtn" src={ply} alt=""></img>
-                        <br/>
-                    </a>
-                </div>
-                    <a style={{paddingTop:"15px"}}href="/video/example" class="d-block mb-4 h-100">
-                    <span className="span-title-video" style={{fontFamily:"Montserrat",fontWeight:"500"}}>2. Components</span>
-                    <span className="span-time" style={{fontSize:"13px"}}><span className="far fa-clock" style={{opacity: 0.4,marginLeft:"75px",color:"red"}}></span> 04:32 </span>
-                    </a>
-                </div>
+            {this.state.listvideo.map((item,key)=>{
+                if(item.type==="video"){
+                return(
                 <div class="col-lg-3 col-md-4 col-6">
-                    <div className="d-flex justify-content-center listA">
+                    <div className="d-flex justify-content-center" style={{height:"165px",width:"260px",background:"linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url("+item.image+")"}}>
+                    
                         <a href="/video/example" class="d-block mb-4 h-100">
-                        <img class="img-fluid img-thumbnail plybtn" src={ply} alt=""></img>                        </a>
+                            <img class="img-fluid img-thumbnail plybtn" src={ply} alt=""></img>
+                            <br/>
+                        </a>
                     </div>
                     <a style={{paddingTop:"15px"}} href="/video/example" class="d-block mb-4 h-100">
-                    <span className="span-title-video" style={{fontFamily:"Montserrat",fontWeight:"500"}}>3. Instalasi</span>
-                    <span className="span-time" style={{fontSize:"13px"}}><span className="far fa-clock" style={{opacity: 0.4,marginLeft:"115px",color:"red"}}></span> 04:32 </span>
+                        <span className="span-title-video" style={{fontFamily:"Montserrat",fontWeight:"500"}}>{item.tittle}</span>
+                        <span className="span-time" style={{fontSize:"13px"}}><span className="far fa-clock" style={{opacity:0.4,marginLeft:"96px",color:"red"}}></span> 04:32 </span>
                     </a>
                 </div>
-                <div class="col-lg-3 col-md-4 col-6">
-                    <div className="d-flex justify-content-center listB">
-                        <a href="/video/example" class="d-block mb-4 h-100">
-                        <img class="img-fluid img-thumbnail plybtn" src={ply} alt=""></img>                        </a>
-                    </div>
-                    <a style={{paddingTop:"15px"}} href="/detailvideo" class="d-block mb-4 h-100">
-                    <span className="span-title-video" style={{fontFamily:"Montserrat",fontWeight:"500"}}>4. Character</span>
-                    <span className="span-time" style={{fontSize:"13px"}}><span className="far fa-clock" style={{opacity: 0.4,marginLeft:"103px",color:"red"}}></span> 04:32 </span>
-                    </a>
-                </div>
-                <div class="col-lg-3 col-md-4 col-6">
-                    <div className="d-flex justify-content-center quiiiz">
-                        <a href="/detailquiz" class="d-block mb-4 h-100">
+                
+                )
+                }
+                else if(item.type==="quiz"){
+                    return (
+                        <div class="col-lg-3 col-md-4 col-6">
+                    <div className="d-flex justify-content-center " style={{height:"165px",width:"260px",background:"linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url("+item.image+")"}}>
+                        <a href="/quiz/example" class="d-block mb-4 h-100">
                         <img class="img-fluid img-thumbnail" style={{opacity: 0.4,background: 'transparent', width: '150px', border: 'none'}} src="http://www.stickpng.com/assets/images/5a461410d099a2ad03f9c998.png" alt=""></img>
                         </a>
                     </div>
                     <a style={{paddingTop:"15px"}} href="/quiz/example" class="d-block mb-4 h-100">
-                    <span className="span-title-video"style={{fontFamily:"Montserrat",fontWeight:"500"}}>Quiz Minggu 1</span>
+                    <span className="span-title-video"style={{fontFamily:"Montserrat",fontWeight:"500"}}>{item.tittle}</span>
                     <span className="span-time" style={{fontSize:"13px",marginLeft:"85px",paddingBottom:"1px",color:"#00DFEB"}}>Level <img src={lvl} style={{width:"13px"}}></img>  </span>
                     </a>
                 </div>
-                <div class="col-lg-3 col-md-4 col-6">
-                    <div className="d-flex justify-content-center listA">
-                        <a href="/detailvideo" class="d-block mb-4 h-100">
-                        <img class="img-fluid img-thumbnail plybtn" src={ply} alt=""></img>                            <br/>
-                        </a>
-                    </div>
-                    <a style={{paddingTop:"15px"}} href="/video/example" class="d-block mb-4 h-100">
-                    <span className="span-title-video"style={{fontFamily:"Montserrat",fontWeight:"500"}}>1. Perkenalan</span>
-                    <span className="span-time" style={{fontSize:"13px"}} ><span className="far fa-clock" style={{opacity:0.4 ,marginLeft:"96px",paddingBottom:"1px",color:"red"}}></span> 04:32 </span>
-                    </a>
-                </div>
-                <div class="col-lg-3 col-md-4 col-6">
-                    <div className="d-flex justify-content-center listB">
-                        <a href="/detailvideo" class="d-block mb-4 h-100">
-                        <img class="img-fluid img-thumbnail plybtn" src={ply} alt=""></img>                        </a>
-                    </div>
-                    <a style={{paddingTop:"15px"}} href="/video/example" class="d-block mb-4 h-100">
-                    <span className="span-title-video"style={{fontFamily:"Montserrat",fontWeight:"500"}}>2. Components</span>
-                    <span className="span-time" style={{fontSize:"13px"}}><span className="far fa-clock" style={{opacity: 0.4,marginLeft:"75px",paddingBottom:"1px",color:"red"}}></span> 04:32 </span>
-                    </a>
-                </div>
-                <div class="col-lg-3 col-md-4 col-6">
-                    <div className="d-flex justify-content-center listA">
-                        <a href="/detailvideo" class="d-block mb-4 h-100">
-                        <img class="img-fluid img-thumbnail plybtn" src={ply} alt=""></img>                        </a>
-                    </div>
-                    <a style={{paddingTop:"15px"}} href="/video/example" class="d-block mb-4 h-100">
-                    <span className="span-title-video"style={{fontFamily:"Montserrat",fontWeight:"500"}}>3. Instalasi</span>
-                    <span className="span-time" style={{fontSize:"13px"}}><span className="far fa-clock" style={{opacity: 0.4,marginLeft:"115px",paddingbottom:"1px",color:"red"}}></span> 04:32 </span>
-                    </a>
-                </div>  
+                    )
+                    }
+            })}
             </div>
-            <div></div>
+            <div>
+            </div>
             <div>
                 <br/>
             <hr class="mt-2 mb-5"></hr>
                 <h2 className="h2week2" style={{fontFamily:"Montserrat",fontWeight:"700"}}>Minggu 2</h2>
                 <br/>
-                
-                {/* <div stlye={{width:"20px"}} class="alert alert-danger" role="alert">
-                <div style={{marginRight:"10px"}} className="fas fa-lock"></div>
-                 Konten Terkunci,silahkan selesaikan semua kuis di minggu 1
-                
-                </div> */}
             </div> 
             <div class="row" style={{height:"1px",width:"3440px"}}>
             <div style={{backgroundcolor:"black",height:"442px",width:"1470px"}} class="col-sm-4"><div className="bg-overlay">
